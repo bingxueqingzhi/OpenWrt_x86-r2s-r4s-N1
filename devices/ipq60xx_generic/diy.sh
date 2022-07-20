4cd121807d769b9bb8279a5b702f4d30e46a51c2
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-07-21 03:42:51 +0800

    update

--- devices/ipq60xx_generic/diy.sh
+++ devices/ipq60xx_generic/diy.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 shopt -s extglob
 
-svn export --force https://github.com/openwrt/openwrt/branches/openwrt-22.03/target/imagebuilder target/imagebuilder
+rm -rf package/boot/uboot-envtools package/kernel package/firmware/ath11k* package/qca target/linux/generic target/linux/ipq60xx toolchain
+svn export --force https://github.com/kiddin9/openwrt-ax1800/trunk/package/boot/uboot-envtools package/boot/uboot-envtools
+svn export --force https://github.com/kiddin9/openwrt-ax1800/trunk/package/firmware/ath11k-firmware package/firmware/ath11k-firmware
+svn export --force https://github.com/kiddin9/openwrt-ax1800/trunk/package/qca package/qca
 
-rm -rf devices/common/patches/{targets.patch,usb.patch}
-echo "KERNEL=4.4.60" >> $GITHUB_ENV || true
+svn co https://github.com/kiddin9/openwrt-ax1800/trunk/target/linux/generic target/linux/generic
+svn co https://github.com/kiddin9/openwrt-ax1800/trunk/target/linux/ipq60xx target/linux/ipq60xx
+svn co https://github.com/kiddin9/openwrt-ax1800/trunk/dl dl
+svn co https://github.com/openwrt/openwrt/branches/openwrt-21.02/toolchain toolchain
+svn co https://github.com/openwrt/openwrt/branches/openwrt-21.02/package/kernel package/kernel
+rm -rf target/linux/generic/files
 
-svn co https://github.com/openwrt/openwrt/branches/openwrt-22.03/package/libs package/libs
-svn co https://github.com/openwrt/openwrt/branches/openwrt-22.03/package/utils package/utils
-
-rm -rf feeds/packages/net/gnunet*
-./scripts/feeds update -a
-./scripts/feeds install -a -p kiddin9 -f
-./scripts/feeds install -a
+curl -sfL https://raw.githubusercontent.com/tsingui/openwrt-ax1800/master/include/netfilter.mk -o include/netfilter.mk
+curl -sfL https://raw.githubusercontent.com/tsingui/openwrt-ax1800/master/include/quilt.mk -o include/quilt.mk
+curl -sfL https://raw.githubusercontent.com/tsingui/openwrt-ax1800/master/include/target.mk -o include/target.mk
 
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += luci-app-cpufreq automount/' target/linux/ipq60xx/Makefile
-
 
 echo '
 CONFIG_ARM64_CRYPTO=y
@@ -41,4 +43,15 @@ CONFIG_CPU_FREQ_GOV_ONDEMAND=y
 CONFIG_CPU_FREQ_GOV_CONSERVATIVE=y
 CONFIG_MOTORCOMM_PHY=y
 CONFIG_SENSORS_PWM_FAN=y
-' >> ./target/linux/ipq60xx/config-4.14
+CONFIG_ACPI=n
+CONFIG_PNP_DEBUG_MESSAGES=y
+CONFIG_PINCTRL_BAYTRAIL=n
+CONFIG_PINCTRL_CHERRYVIEW=n
+CONFIG_PINCTRL_BROXTON=n
+CONFIG_PINCTRL_SUNRISEPOINT=n
+CONFIG_PINCTRL_QDF2XXX=n
+CONFIG_GPIO_AMDPT=n
+CONFIG_PCC=n
+CONFIG_PMIC_OPREGION=n
+CCONFIG_RYPTO_CRC32_ARM64=n
+' >> ./target/linux/ipq60xx/config-4.4
